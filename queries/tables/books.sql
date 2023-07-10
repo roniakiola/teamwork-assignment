3adc51870254dfa0fa98615d8fec39000e18518f
Rich <applerich0306@gmail.com>
2023-07-10 23:47:05 +0300

    clean spcae

--- queries/tables/books.sql
+++ queries/tables/books.sql
@@ -1,26 +1,3 @@
--- Table: application.books
-
--- DROP TABLE IF EXISTS application.books;
-
--- CREATE TABLE IF NOT EXISTS application.books
--- (
---     id integer NOT NULL DEFAULT nextval('application.books_id_seq'::regclass),
---     book_name character varying(50) COLLATE pg_catalog."default",
---     isbn character varying(13) COLLATE pg_catalog."default",
---     publishing_year integer,
---     description character varying(1000) COLLATE pg_catalog."default",
---     image_url character varying COLLATE pg_catalog."default",
---     borrowing_period integer,
---     quantity integer,
---     CONSTRAINT books_pkey PRIMARY KEY (id)
--- )
-
--- TABLESPACE pg_default;
-
--- ALTER TABLE IF EXISTS application.books
---     OWNER to postgres;
-
-
 CREATE TABLE IF NOT EXISTS public.books
 (
     id integer GENERATED ALWAYS AS IDENTITY,
--- queries/tables/genres.sql
+++ queries/tables/genres.sql
@@ -1,20 +1,3 @@
--- Table: application.genres
-
--- DROP TABLE IF EXISTS application.genres;
-
--- CREATE TABLE IF NOT EXISTS application.genres
--- (
---     id integer NOT NULL DEFAULT nextval('application.genres_id_seq'::regclass),
---     genre_name character varying(30) COLLATE pg_catalog."default",
---     CONSTRAINT genres_pkey PRIMARY KEY (id)
--- )
-
--- TABLESPACE pg_default;
-
--- ALTER TABLE IF EXISTS application.genres
---     OWNER to admin;
-
-
 CREATE TABLE IF NOT EXISTS public.genres
 (
     id integer NOT NULL,
