4c424456a6ce98854cc14522d1f7cde1f7f12bb6
Rich <applerich0306@gmail.com>
2023-07-10 23:44:17 +0300

    clean empty space

--- queries/tables/authors.sql
+++ queries/tables/authors.sql
@@ -1,20 +1,3 @@
--- Table: application.authors
-
--- DROP TABLE IF EXISTS application.authors;
-
--- CREATE TABLE IF NOT EXISTS application.authors
--- (
---     id integer NOT NULL DEFAULT nextval('application.authors_id_seq'::regclass),
---     author_name character varying(30) COLLATE pg_catalog."default",
---     CONSTRAINT authors_pkey PRIMARY KEY (id)
--- )
-
--- TABLESPACE pg_default;
-
--- ALTER TABLE IF EXISTS application.authors
---     OWNER to admin;
-
-
 CREATE TABLE IF NOT EXISTS public.authors
 (
     id integer NOT NULL,
