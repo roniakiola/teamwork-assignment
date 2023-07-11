9ae6d32a2600fcf92537ad7b9b42421073b080ee
Roni Akiola <roni.akiola@hotmail.com>
2023-07-11 23:59:28 +0300

    delete book to return true

--- bookFunctions/deleteBook.sql
+++ bookFunctions/deleteBook.sql
@@ -1,12 +1,12 @@
 --Delete book
 CREATE OR REPLACE FUNCTION delete_book(p_book_id INT)
-RETURNS VOID
+RETURNS BOOLEAN
 AS $$
 BEGIN
   IF NOT EXISTS (SELECT 1 FROM books WHERE id = p_book_id) THEN
     RAISE EXCEPTION 'Book with ID % does not exist.', p_book_id;
   END IF;
-  
+
   DELETE FROM book_author
   WHERE book_id = p_book_id;
 
@@ -15,6 +15,8 @@ BEGIN
 
   DELETE FROM books
   WHERE id = p_book_id;
+  
+  RETURN TRUE;
 END;
 $$
 LANGUAGE plpgsql;
