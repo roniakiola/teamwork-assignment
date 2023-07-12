3121f8a6ec9d19f307f47cab6758807f5ff459cb
Rich <applerich0306@gmail.com>
2023-07-12 16:47:54 +0300

    delete but back loans and fines folder

--- queries/functions/fine/get_fine_by_id.sql
+++
@@ -1,3 +0,0 @@
-SELECT id, created_date, due_date, payment_date, balance, loan_id
-FROM fine
-WHERE id = 1;
--- queries/functions/loans/get_loan_by_id.sql
+++
@@ -1,3 +0,0 @@
-SELECT id, borrowed_date, returned_date, due_date, book_id, user_id
-FROM loans
-WHERE id = 1;
