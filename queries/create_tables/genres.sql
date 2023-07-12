8e0562076efbc7b122ab917b260eadff8e96b893
Rich <applerich0306@gmail.com>
2023-07-12 16:27:49 +0300

    integrate create tables

--- queries/create_tables/genres.sql
+++ queries/create_tables/genres.sql
@@ -3,8 +3,4 @@ CREATE TABLE IF NOT EXISTS public.genres
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
     genre_name character varying(30) COLLATE pg_catalog."default",
     CONSTRAINT genres_pkey PRIMARY KEY (id)
-)
--- ALTER TABLE public.genres
--- ALTER COLUMN id SET DATA TYPE integer,
--- ALTER COLUMN id SET NOT NULL,
--- ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+);
